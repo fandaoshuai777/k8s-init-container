3d8b9592756712b92d51ece939d110a28d5727a2
liuning1234561 <liuning123456>
2022-06-03 20:19:11 +0800

    日账单列表接口联调

--- src/views/bill/components/sysdialog.vue
+++ src/views/bill/components/sysdialog.vue
@@ -1,105 +1,96 @@
 <template>
-  <div>
-    <el-dialog
-      title="关联订单"
-      :visible.sync="dialogVisible"
-      width="80%"
-      :before-close="handleClose"
-    >
-      <el-form ref="searchForm" :model="searchForm" :inline="true">
-        <el-form-item label="用户手机号" prop="stationName">
-          <el-input v-model="searchForm.phone" placeholder="" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" class="adds" @click="doSearch">搜索</el-button>
-        </el-form-item>
-      </el-form>
-      <!-- 表单 -->
-      <el-table
-        :data="tableData"
-        border
-        :header-cell-style="{ background: '#eff3f6' }"
-        style="width: 100%"
-      >
-        <el-table-column
-          prop="date"
-          label="编号"
-        />
-        <el-table-column
-          prop="name"
-          label="订单编号"
-        />
-        <el-table-column
-          prop="address"
-          label="付款用户"
-        />
-        <el-table-column
-          prop="address"
-          label="订单金额"
-        />
-        <el-table-column
-          prop="address"
-          label="员工卡金额"
-        />
-        <el-table-column
-          prop="address"
-          label="优惠券面额"
-        />
-        <el-table-column
-          prop="address"
-          label="优惠券类型编码"
-        />
-        <el-table-column
-          prop="address"
-          label="交易时间"
-        />
-      </el-table>
-      <!-- 分页 -->
-      <el-pagination
-        :current-page="pagination.current"
-        :page-sizes="[10, 20, 30, 40]"
-        :page-size="pagination.pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-      />
-    </el-dialog>
-  </div>
+	<div>
+		<el-dialog title="关联订单" :visible.sync="dialogVisible" width="80%" :before-close="handleClose">
+			<el-form ref="searchForm" :model="searchForm" :inline="true">
+				<el-form-item label="用户手机号" prop="driverTel">
+					<el-input v-model="searchForm.driverTel" placeholder="" />
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" class="adds" @click="doSearch">搜索</el-button>
+				</el-form-item>
+			</el-form>
+			<!-- 表单 -->
+			<el-table v-loading="loading" :data="tableData" border :header-cell-style="{ background: '#eff3f6' }" style="width: 100%">
+				<el-table-column prop="ids" label="编号" />
+				<el-table-column prop="orderNo" label="订单编号" />
+				<el-table-column prop="driverTel" label="付款用户" />
+				<el-table-column prop="billingAmount" label="订单金额" />
+				<el-table-column prop="oneCardAmount" label="一卡通金额" />
+				<el-table-column prop="employeeCardAmount" label="员工卡金额" />
+				<el-table-column prop="couponsAmount" label="优惠券面额" />
+				<el-table-column prop="couponNo" label="优惠券类型编码" />
+				<el-table-column prop="paymentTime" label="交易时间" />
+			</el-table>
+			<!-- 分页 -->
+			<el-pagination
+				:current-page="pagination.pageIndex"
+				:page-sizes="[10, 20, 30, 40]"
+				:page-size="pagination.pageSize"
+				layout="total, sizes, prev, pager, next, jumper"
+				:total="total"
+				@size-change="handleSizeChange"
+				@current-change="handleCurrentChange"
+			/>
+		</el-dialog>
+	</div>
 </template>
 
 <script>
+import { commitOrder } from '@/api/billDj';
 export default {
-  data() {
-    return {
-      dialogVisible: false,
-      searchForm: {
-        phone: ''
-      },
-      loading: false,
-      tableData: [{ billNo: 1 }], // 列表
-      pagination: {
-        pageSize: 10,
-        currPage: 1
-      },
-      total: 0
-    }
-  },
-  methods: {
-    doSearch() {
-
+	data() {
+		return {
+			dialogVisible: false,
+			searchForm: {
+				driverTel: '',
+			},
+			billNo: null,
+			loading: true,
+			tableData: [], // 列表
+			pagination: {
+				pageSize: 10,
+				pageIndex: 1,
+			},
+			total: 0,
+		};
+	},
+	methods: {
+		doSearch() {
+      this.getList()
     },
-    handleClose() {
-      this.dialogVisible = false
-    },
-    handleSizeChange(val) {
-      console.log(`每页 ${val} 条`)
-    },
-    handleCurrentChange(val) {
-      console.log(`当前页: ${val}`)
-    }
-  }
-}
+		getList() {
+			const data = { ...this.searchForm, ...this.pagination };
+			data.billNo = this.billNo;
+			this.loading = true;
+			commitOrder(data).then((res) => {
+				if (res.code === 200) {
+					this.tableData = res.result.map((item) => {
+						item.ids = this.pagination.pageIndex * this.pagination.pageSize - 9;
+						return {
+							...item,
+						};
+					});
+					this.loading = false;
+					this.total = res.data.stationDailyBillCardDto.totalCount;
+				}
+			});
+		},
+		handleClose() {
+			this.dialogVisible = false;
+		},
+		handleSizeChange(val) {
+			this.loading = true;
+			this.pagination.pageSize = val;
+			this.pagination.pageIndex = 1;
+			this.getList();
+		},
+		handleCurrentChange(val) {
+			this.loading = true;
+			this.pagination.pageIndex = val;
+			this.getList();
+		},
+	},
+};
 </script>
 
 <style>
--- src/views/bill/index.vue
+++ src/views/bill/index.vue
@@ -1,94 +1,124 @@
 <template>
-	<div>
-		<el-card shadow="hover">
-			<!-- 搜索 -->
-			<el-form ref="searchForm" :model="searchForm" label-width="120px" :inline="true">
-				<el-form-item label="账单日期" prop="stationName">
-					<el-date-picker v-model="searchForm.date" type="date" placeholder="选择日期" />
-				</el-form-item>
-				<el-form-item label="数据是否为0" prop="stationName">
-					<el-select v-model="searchForm.select">
-						<el-option label="数据不为0" :value="0" />
-						<el-option label="全部" :value="1" />
-					</el-select>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" class="adds" @click="doSearch">搜索</el-button>
-				</el-form-item>
-			</el-form>
-			<!-- 表单 -->
-			<el-table :data="tableData" border  style="width: 100%">
-				<el-table-column prop="date" label="编号" width="180" />
-				<el-table-column prop="name" label="商户编号" width="180" />
-				<el-table-column prop="address" label="商户名称" />
-				<el-table-column prop="address" label="订单总额" />
-				<el-table-column prop="address" label="一卡通总额" />
-				<el-table-column prop="address" label="员工总额" />
-				<el-table-column prop="address" label="券总额" />
-				<el-table-column prop="address" label="账单日期" />
-				<el-table-column label="操作">
-					<template slot-scope="scope">
-						<el-button type="text" @click="Associated(scope.row)">关联订单</el-button>
-					</template>
-				</el-table-column>
-			</el-table>
-			<!-- 分页 -->
-			<el-pagination
-				:current-page="pagination.current"
-				:page-sizes="[10, 20, 30, 40]"
-				:page-size="pagination.pageSize"
-				layout="total, sizes, prev, pager, next, jumper"
-				:total="total"
-				@size-change="handleSizeChange"
-				@current-change="handleCurrentChange"
-			/>
-			<!-- 弹窗 -->
-			<Sysdialog ref="Associated" />
-		</el-card>
+	<div class="Main">
+		<!-- 搜索 -->
+		<el-form ref="searchForm" :model="searchForm" label-width="120px" :inline="true">
+			<el-form-item label="账单日期" prop="billTime">
+				<el-date-picker v-model="searchForm.billTime" type="date" placeholder="选择日期" />
+			</el-form-item>
+			<el-form-item label="数据是否为0" prop="amountFlag">
+				<el-select v-model="searchForm.amountFlag">
+					<el-option label="全部" :value="0" />
+					<el-option label="数据不为0" :value="1" />
+					<el-option label="数据为0" :value="2" />
+				</el-select>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" class="adds" @click="doSearch">搜索</el-button>
+			</el-form-item>
+		</el-form>
+		<!-- 表单 -->
+		<el-table v-loading="loading" :data="tableData" border style="width: 100%">
+			<el-table-column prop="ids" label="编号" width="180" />
+			<el-table-column prop="stationId" label="商户编号" width="180" />
+			<el-table-column prop="stationName" label="商户名称" />
+			<el-table-column prop="billingAmount" label="订单总额" />
+			<el-table-column prop="oneCardAmount" label="一卡通总额" />
+			<el-table-column prop="employeeCardAmount" label="员工总额" />
+			<el-table-column prop="couponsAmount" label="券总额" />
+			<el-table-column prop="billTime" label="账单日期" />
+			<el-table-column label="操作">
+				<template slot-scope="scope">
+					<el-button type="text" @click="Associated(scope.row)">关联订单</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+		<!-- 分页 -->
+		<el-pagination
+			:current-page="pagination.current"
+			:page-sizes="[10, 20, 30, 40]"
+			:page-size="pagination.pageSize"
+			layout="total, sizes, prev, pager, next, jumper"
+			:total="total"
+			@size-change="handleSizeChange"
+			@current-change="handleCurrentChange"
+		/>
+		<!-- 弹窗 -->
+		<Sysdialog ref="Associated" />
 	</div>
 </template>
 
 <script>
-import Sysdialog from './components/sysdialog.vue'
+import Sysdialog from './components/sysdialog.vue';
+import { billList } from '@/api/billDj';
 export default {
-  components: {
-    Sysdialog
-  },
-  data() {
-    return {
-      searchForm: {
-        date: '',
-        select: 0
-      },
-      loading: false,
-      tableData: [{ billNo: 1 }], // 列表
-      pagination: {
-        pageSize: 10,
-        currPage: 1
-      },
-      total: 0
-    }
-  },
-  methods: {
-    // 搜索
-    doSearch() {},
-    // 关联订单
-    Associated() {
-      this.$refs.Associated.dialogVisible = true
-    },
-    handleSizeChange() {
-    //   console.log(`每页 ${val} 条`)
-    },
-    handleCurrentChange() {
-    //   console.log(`当前页: ${val}`)
-    }
-  }
-}
+	components: {
+		Sysdialog,
+	},
+	data() {
+		return {
+			searchForm: {
+				billTime: '',
+				amountFlag: 0,
+			},
+			loading: true,
+			tableData: [{ billNo: 1 }], // 列表
+			pagination: {
+				pageSize: 10,
+				currPage: 1,
+			},
+			total: 0,
+		};
+	},
+	created() {
+		this.init();
+	},
+	methods: {
+		// 搜索
+		doSearch() {
+			this.init();
+		},
+		// 获取列表
+		init() {
+			const data = this.searchForm;
+			// let stationId = window.sessionStorage.getItem('enterpriseId')
+			// data.stationId = stationId
+			data.stationId = 1;
+			this.loading = true;
+			billList(data).then((res) => {
+				if (res.code === 200) {
+					if (res.result.length > 0) {
+						this.tableData = res.result.map((item) => {
+							item.ids = this.pagination.currPage * this.pagination.pageSize - 9;
+							return {
+								...item,
+							};
+						});
+					} else {
+						this.tableData = [];
+					}
+					this.loading = false;
+				}
+			});
+		},
+		// 关联订单
+		Associated(row) {
+			this.$refs.Associated.billNo = row.billNo;
+			this.$refs.Associated.dialogVisible = true;
+			this.$refs.Associated.getList();
+		},
+		handleSizeChange() {
+			//   console.log(`每页 ${val} 条`)
+		},
+		handleCurrentChange() {
+			//   console.log(`当前页: ${val}`)
+		},
+	},
+};
 </script>
 
 <style scoped>
-::v-deep .el-pagination{
-    text-align: center;
-    padding-top: 20px;
+::v-deep .el-pagination {
+	text-align: center;
+	padding-top: 20px;
 }
 </style>
