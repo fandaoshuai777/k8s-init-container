52f92d87d84a8cee4ebd6548ddfc05428a93192d
李小龙 <lixiaolong@cngotone.com>
2022-08-17 10:26:44 +0800

    域名更换

--- src/utils/requstGatway.js
+++ src/utils/requstGatway.js
@@ -6,11 +6,11 @@ import { Local, Session } from '@/utils/storage';
 
 function baseURLEnv() {
     if (process.env.NODE_ENV === 'development') {
-        return 'https://internal.fat.op-api-gateway.wonder-link.net/'
-    } else if (window.location.hostname.indexOf('ciecdev') !== -1) {
-        return 'https://internal.dev.op-api-gateway.wonder-link.net'
+        return 'https://internal.fat.op-api-gateway.wonder-link.net'
+    } else if (window.location.hostname.indexOf('test') !== -1) {
+        return 'https://internal.fat.op-api-gateway.wonder-link.net'
     } else if (window.location.hostname.indexOf('fat') !== -1) {
-        return 'https://internal.fat.op-api-gateway.wonder-link.net/'
+        return 'https://internal.fat.op-api-gateway.wonder-link.net'
     } else if (window.location.hostname.indexOf('fat-1') !== -1) {
         return 'https://internal.fat-1.op-api-gateway.wonderlink.cc'
     } else {
