3f9d03227792b225d6103bfcbbf99b331529e52b
李小龙 <lixiaolong@cngotone.com>
2022-08-17 17:43:39 +0800

    切换fat-1环境

--- src/utils/orderRequest.js
+++ src/utils/orderRequest.js
@@ -24,11 +24,11 @@ function baseURLEnv() {
 		return 'https://internal.dev.op-api-gateway.wonder-link.net'
 	} else if (window.location.hostname.indexOf('test') !== -1) {
 		return 'https://internal.fat.op-api-gateway.wonder-link.net'
-	} else if (window.location.hostname.indexOf('fat') !== -1) {
-		return 'https://internal.fat.op-api-gateway.wonder-link.net'
 	} else if (window.location.hostname.indexOf('fat-1') !== -1) {
 		return 'http://internal.fat-1.merchant-server.wonderlink.cc'
-	}else {
+	} else if (window.location.hostname.indexOf('fat') !== -1) {
+		return 'https://internal.fat.op-api-gateway.wonder-link.net'
+	} else {
 		return 'https://op-api-gateway.wonder-link.net'
 	}
 }
