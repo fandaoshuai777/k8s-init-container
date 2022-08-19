a07d54f80bec6cfe1ac41d914235afd9c54ed114
李小龙 <lixiaolong@cngotone.com>
2022-08-19 10:23:16 +0800

    下载中心联调完

---
+++ src/api/oil/download.js
@@ -0,0 +1,18 @@
+import request from '@/utils/requstGatway';
+
+//下载中心列表
+export function selectPage(data) {
+    return request({
+        url: `/gotone-cms-api/mp/async/export/select_page`,
+        method: 'post',
+        data
+    })
+}
+//删除
+export function deleteById(data) {
+    return request({
+        url: `/gotone-cms-api/mp/async/export/delete_by_id`,
+        method: 'post',
+        data
+    })
+}
--- src/views/orderManagement/downloadCenter.vue
+++ src/views/orderManagement/downloadCenter.vue
@@ -1,12 +1,12 @@
 <template>
 	<div>
 		<el-card shadow="hover">
-			<el-form :model="formInline" label-width="95px" :inline="true" label-position="right" ref="reset">
+			<el-form :model="formInline" label-width="95px" :inline="true" label-position="right" ref="reset" size="small">
 				<el-form-item label="报表名称">
-					<el-input v-model="formInline.id" @input.native="changeNum" placeholder="请输入报表名称" clearable> </el-input>
+					<el-input v-model="formInline.fileName" placeholder="请输入报表名称" clearable> </el-input>
 				</el-form-item>
 				<el-form-item>
-					<el-button type="primary" @click="inquire">查询</el-button>
+					<el-button type="primary" @click="inquire" size="small">查询</el-button>
 				</el-form-item>
 			</el-form>
 			<Table
@@ -27,6 +27,8 @@
 </template>
 <script>
 import Table from '@/components/table/index_detail';
+import { Session } from '@/utils/storage';
+import { selectPage, deleteById } from '@/api/oil/download';
 export default {
 	components: {
 		Table,
@@ -34,26 +36,31 @@ export default {
 	data() {
 		return {
 			formInline: {
-				id: '',
+				fileName: '',
 			},
-			loading: false,
-			tableData: [{ a: 1, b: 2, c: 3, d: 4 }],
+			pagination: {
+				pageSize: 10,
+				currPage: 1,
+			},
+			total: 0,
+			loading: true,
+			tableData: [],
 			colums: [
 				{
 					label: '报表名称',
-					prop: 'a',
+					prop: 'fileName',
 				},
 				{
 					label: '导出时间',
-					prop: 'b',
+					prop: 'exportTime',
 				},
 				{
 					label: '操作人',
-					prop: 'c',
+					prop: 'operatorName',
 				},
 				{
 					label: '状态',
-					prop: 'd',
+					prop: 'stats',
 				},
 				{
 					label: '操作',
@@ -68,15 +75,78 @@ export default {
 			return { ...this.pagination, total: this.total };
 		},
 	},
+	mounted() {
+		this.downloadList();
+	},
 	methods: {
 		// 查询
-		inquire() {},
+		inquire() {
+			this.loading = true;
+			this.pagination.currPage = 1;
+			this.downloadList();
+		},
 		// 删除
-		remove() {},
+		remove(row) {
+			this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+				confirmButtonText: '确定',
+				cancelButtonText: '取消',
+				type: 'warning',
+			})
+				.then(() => {
+					const data = {
+						id: row.id,
+						operatorId: Session.get('loginUser').userId,
+						operatorName: Session.get('loginUser').userName,
+					};
+					this.deleteList(data);
+				})
+				.catch(() => {
+					this.$message({
+						type: 'info',
+						message: '已取消删除',
+					});
+				});
+		},
+		deleteList() {
+			deleteById(data).then((res) => {
+				if (res.code === '0') {
+					this.$message.success('删除成功');
+					this.downloadList();
+				} else {
+					this.$message.error(res.message);
+					this.downloadList();
+				}
+			});
+		},
 		// 下载
-		download() {},
+		download(row) {
+			let link = document.createElement('a');
+			link.style.display = 'none';
+			link.href = row.fileUrl;
+			// link.setAttribute('download');
+			document.body.appendChild(link);
+			link.click();
+		},
 		// 列表
-		downloadList() {},
+		downloadList() {
+			const data = {
+				fileName: this.formInline.fileName,
+				merchantId: Session.get('merchantId'),
+				pageNo: this.pagination.currPage,
+				pageSize: this.pagination.pageSize,
+			};
+			selectPage(data).then((res) => {
+				this.loading = false;
+				this.total = Number(res.data.total);
+				this.tableData = res.data.list.map((n) => {
+					return {
+						...n,
+						stats:
+							n.stats === 1001 ? '未处理' : n.stats === 1002 ? '处理中' : n.stats === 1003 ? '导出失败' : n.stats === 1004 ? '导出成功' : n.stats,
+					};
+				});
+			});
+		},
 		// 页码变化
 		paginationChange(value) {
 			this.pagination.currPage = value.current;
