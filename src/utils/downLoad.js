be477d6a40772f8dc4ecea6ef185fdfe9401992f
李小龙 <lixiaolong@cngotone.com>
2022-09-01 14:54:14 +0800

    下载线上域名修改

--- src/utils/downLoad.js
+++ src/utils/downLoad.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 function baseURLEnv() {
   if (process.env.NODE_ENV === 'development') {
     // return 'https://internal.dev.op-api-gateway.wonder-link.net'
-    return ' https://internal.fat-1.op-api-gateway.wonderlink.cc'
+    return 'http://192.168.3.73:80'
 
   } else if (window.location.hostname.indexOf('fat-1') !== -1) {
     return ' https://internal.fat-1.op-api-gateway.wonderlink.cc'
   } else {
-    return 'http://merchant-server.cngotone.com'
+    return 'https://op-api-gateway.wonder-link.net'
   }
 }
 export const downloadBlob = (fileName, url) => {
@@ -17,14 +17,14 @@ export const downloadBlob = (fileName, url) => {
     function (response) {
 console.log(response.headers['content-disposition'].split('.')[1])
 console.log(fileName)
-      const link = document.createElement('a')
-      const blob = new Blob([response.data])
-      link.style.display = 'none'
-      link.href = URL.createObjectURL(blob)
-      link.setAttribute('download', fileName +'.'+ response.headers['content-disposition'].split('.')[1])
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
+      // const link = document.createElement('a')
+      // const blob = new Blob([response.data])
+      // link.style.display = 'none'
+      // link.href = URL.createObjectURL(blob)
+      // link.setAttribute('download', fileName +'.'+ response.headers['content-disposition'].split('.')[1])
+      // document.body.appendChild(link)
+      // link.click()
+      // document.body.removeChild(link)
     }
 
    
