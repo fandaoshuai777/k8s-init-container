f15be22accdbbf732ffc0bd042269c61429f98d4
quhongqiang <1256618908@qq.com>
2022-10-19 11:23:52 +0800

    fix(修改一键加油订单隐藏申请退款按钮fat1环境)

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
 		hotOnly: false, // 热更新
 		proxy: {
 			[process.env.VUE_APP_BASE_API]: {
-				target: 'http://internal.fat-1.merchant-server.wonderlink.cc',
+				target: 'http://internal.fat1.merchant-server.wonderlink.cc',
 				changeOrigin: true,
 				pathRewrite: {
 					['^' + process.env.VUE_APP_BASE_API]: ''
